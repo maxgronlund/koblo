0f878e82ac5077f8fe0ea2b6f5bb00de7f51d338
Synthmax <max@synthmax.dk>
2014-09-16 00:43:48 +0200

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,5 +80,25 @@ namespace :deploy do
   # compile assets locally then rsync
   after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
+  
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  #before 'deploy:setup_config', 'nginx:remove_default_vhost'
+
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  #after 'deploy:setup_config', 'nginx:reload'
+
+  # Restart monit so it will pick up any monit configurations
+  # we've added
+  after 'deploy:setup_config', 'monit:restart'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
+  
+  # copy images without uuid stamp
+  after 'deploy:publishing', 'deploy:copy_images'
+  
 end
 
